e3595ca8ed90e28086e778c7ab1496e0b3136425
tjr <tjroy13june@gmail.com>
2019-01-20 23:27:40 +0600

    simple calculator program with java done

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        Scanner scr=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double a=scr.nextDouble();
+        System.out.println("Enter second Number:");
+        double b=scr.nextDouble();
+        double addition=a+b;
+        double subtraction=a-b;
+        double multiplication=a*b;
+        double division=a/b;
+        System.out.println("Addition of the two number is "+addition);
+        System.out.println("Subtraction of the two number is "+subtraction);
+        System.out.println("Multiplication of the two number is "+multiplication);
+        System.out.println("Division of the two number is "+division);
+
+
+    }
+}
