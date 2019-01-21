47295a46803beb9b4c3c869de377b2e21b2c56fe
tjr <tjroy13june@gmail.com>
2019-01-21 12:25:07 +0600

    Calculator final program with java is completed

--- Calculator.java
+++ Calculator.java
@@ -9,15 +9,32 @@ public class Calculator
         double a=scr.nextDouble();
         System.out.println("Enter second Number:");
         double b=scr.nextDouble();
-        double addition=a+b;
-        double subtraction=a-b;
-        double multiplication=a*b;
-        double division=a/b;
-        System.out.println("Addition of the two number is "+addition);
-        System.out.println("Subtraction of the two number is "+subtraction);
-        System.out.println("Multiplication of the two number is "+multiplication);
-        System.out.println("Division of the two number is "+division);
-        System.out.println("Thanks for using the program");
+        System.out.println("What you want to do with the numbers ?");
+        System.out.println("Press + for Addition");
+        System.out.println("Press - for Subtraction");
+        System.out.println("Press * for Multiplication");
+        System.out.println("Press / for Division");
+        
+        char decision;
+        decision=scr.next().charAt(0);
+        switch(decision)
+        {
+            case '+':
+            System.out.println("Addition of the two number is ="+(a+b));
+            break;
+            case '-':
+            System.out.println("Subtraction of the two number is ="+(a-b));
+            break;
+            case '*':
+            System.out.println("Multiplication of the two number is ="+(a*b));
+            break;
+            case '/':
+            System.out.println("Division of the two number is ="+(a/b));
+            break;
+            default:
+            System.out.println("You may have press wrong button");
+        }
+    
 
 
     }
